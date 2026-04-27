103da16171e145d0f3714ccde32d6184392c80dd
Fllunn <dmitrijpohoda2940@gmail.com>
2026-04-27 23:06:27 +0500

    add type submit for btn

--- app/components/cabinet/settings/Dialog.vue
+++ app/components/cabinet/settings/Dialog.vue
@@ -31,56 +31,60 @@ function close(): void {
     width="clamp(320px, 90vw, 512px)"
     @update:model-value="emit('update:modelValue', $event)"
   >
-    <v-card>
-      <v-card-title class="pa-4">
-        <v-row no-gutters align="center">
-          <v-col cols="2" />
+    <v-form @submit.prevent="emit('submit')">
+      <v-card>
+        <v-card-title class="pa-4">
+          <v-row no-gutters align="center">
+            <v-col cols="2" />
 
-          <v-col cols="8" class="text-center text-wrap">
-            {{ title }}
-          </v-col>
+            <v-col cols="8" class="text-center text-wrap">
+              {{ title }}
+            </v-col>
 
-          <v-col cols="2" class="d-flex justify-end">
-            <v-btn
-              icon="mdi-close"
-              variant="text"
-              size="small"
-              @click="close"
-            />
-          </v-col>
-        </v-row>
-      </v-card-title>
+            <v-col cols="2" class="d-flex justify-end">
+              <v-btn
+                icon="mdi-close"
+                variant="text"
+                size="small"
+                type="button"
+                @click="close"
+              />
+            </v-col>
+          </v-row>
+        </v-card-title>
 
-      <v-card-subtitle
-        v-if="description"
-        class="text-center text-wrap"
-      >
-        {{ description }}
-      </v-card-subtitle>
-
-      <v-card-text class="pb-0 text-center">
-        <slot />
-      </v-card-text>
-
-      <v-card-actions
-        class="justify-center mb-2"
-      >
-        <v-btn
-          variant="text"
-          @click="close"
+        <v-card-subtitle
+          v-if="description"
+          class="text-center text-wrap"
         >
-          Отмена
-        </v-btn>
+          {{ description }}
+        </v-card-subtitle>
 
-        <v-btn
-          color="primary"
-          :loading="loading"
-          :disabled="disabled"
-          @click="emit('submit')"
+        <v-card-text class="pb-0 text-center">
+          <slot />
+        </v-card-text>
+
+        <v-card-actions
+          class="justify-center mb-2"
         >
-          {{ submitText }}
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+          <v-btn
+            variant="text"
+            type="button"
+            @click="close"
+          >
+            Отмена
+          </v-btn>
+
+          <v-btn
+            color="primary"
+            type="submit"
+            :loading="loading"
+            :disabled="disabled"
+          >
+            {{ submitText }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-form>
   </v-dialog>
 </template>
